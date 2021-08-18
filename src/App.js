d05b9d6b7045c150910f6d9e78d392e1b35aa5c8
bionces <jimeshov0@gmail.com>
2021-08-18 16:13:18 +0600

    make map

--- src/App.js
+++ src/App.js
@@ -5,43 +5,31 @@ import { ForSale } from "./view/pages/forSale/ForSale";
 import LearnMore from "./view/pages/learnmore/LearnMore";
 import { Signup } from "./view/components/signup/Signup";
 import { OnMap } from "./view/pages/onMap/OnMap";
-import  {PrivatePoute} from "./view/router/PrivateRoute";
-import {useState} from "react";
-import {PublickRoute} from "./view/router/PublickRoute";
 
 
 
 function App() {
-  const [user, setUser] = useState(()=>JSON.parse(localStorage.getItem("user")))
-  const addUser = (obj)=>{
-    console.log(obj)
-    setUser(obj)
-    localStorage.setItem("user", JSON.stringify(obj))
-  }
   return (
-      <Router>
-        <div className="App">
-          <Header addUser={addUser} user={user}/>
-          <Switch>
-            <PrivatePoute user= {user} path="/dashboard" Component={() =><div>Dashboard</div>}/>
-            <Route exact path="/">
-              <ForSale />
-            </Route>
-
-            <Route exact path="/learnmore/:id">
-              <LearnMore />
-            </Route>
-
-            <PublickRoute user= {user} path="/signin" Component={Signup}/>
-            {/*<Route exact path="/signin">*/}
-            {/*  </>*/}
-            {/*</Route>*/}
-            <Route>
-              <OnMap exact path ="/onmap"/>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <ForSale />
+          </Route>
+         
+          <Route exact path="/learnmore/:id">
+            <LearnMore />
+          </Route>
+          <Route exact path="/signin">
+            <Signup/>
+          </Route>
+          <Route>
+            <OnMap exact path ="/onmap"/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
