b3d05c9ee9d45f118cc9e6adb9f4dd176f9e7aa7
bionces <jimeshov0@gmail.com>
2021-08-18 16:23:14 +0600

    add

--- src/App.js
+++ src/App.js
@@ -5,41 +5,17 @@ import { ForSale } from "./view/pages/forSale/ForSale";
 import LearnMore from "./view/pages/learnmore/LearnMore";
 import { Signup } from "./view/components/signup/Signup";
 import { OnMap } from "./view/pages/onMap/OnMap";
-<<<<<<< HEAD
-=======
+
 import  {PrivatePoute} from "./view/router/PrivateRoute";
 import {useState} from "react";
 import {PublickRoute} from "./view/router/PublickRoute";
 import { Information } from "./view/pages/information/Information";
 
->>>>>>> ad8f952fc35a21821ce500a2f3e36ae261479d74
 
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <ForSale />
-          </Route>
-         
-          <Route exact path="/learnmore/:id">
-            <LearnMore />
-          </Route>
-          <Route exact path="/signin">
-            <Signup/>
-          </Route>
-          <Route>
-            <OnMap exact path ="/onmap"/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-=======
       <Router>
         <div className="App forsale__relative">
           <Header addUser={addUser} user={user}/>
@@ -71,7 +47,6 @@ function App() {
           <a className="whatsapp" href="https://api.whatsapp.com/send/?phone=%2B996702300913&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5+%D1%8F+%D1%81+%D1%81%D0%B0%D0%B9%D1%82%D0%B0%2C+%D0%BC%D0%BE%D0%B3%D1%83+%D0%BB%D0%B8+%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C+%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8E+%3F&app_absent=0"> <img  src="/img/whatsapp.svg" alt="" style={{width:"60px"}}/></a>
         </div>
       </Router>
->>>>>>> ad8f952fc35a21821ce500a2f3e36ae261479d74
   );
 }
 
